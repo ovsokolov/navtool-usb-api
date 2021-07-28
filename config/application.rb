be36d624a7d7fbd5ca5808110504ff7a08a088d1
Oleg Sokolov <olegsokolov@Delilahs-MacBook-Pro.local>
2021-07-28 11:31:17 -0400

    add origin to application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Navtool
     config.api_only = true
 
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
